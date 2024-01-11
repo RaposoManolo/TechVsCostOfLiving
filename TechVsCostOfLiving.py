5f2190811e06511000465196276f53edb9e31b94
RaposoManolo <47564554+RaposoManolo@users.noreply.github.com>
2024-01-11 15:04:30 +0000

    Intro to scatter chart

--- TechVsCostOfLiving.py
+++ TechVsCostOfLiving.py
@@ -1,5 +1,23 @@
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('TechCostOfLiving.csv')
 
-print(df.to_string())  
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+columns = ["Country", "Avg Salary", "Cost of Living Index"]
+
+df = pd.read_csv("TechCostOfLiving.csv", usecols=columns)
+df.columns = df.columns.str.replace(' ', '')
+print("Contents in csv file:", df)
+plt.scatter(df.CostofLivingIndex, df.AvgSalary)
+plt.show()
+
+
+#df = pd.read_csv('TechCostOfLiving.csv')
+
+#df.columns = df.columns.str.replace(' ', '') 
+
+#print(df.to_string())  
