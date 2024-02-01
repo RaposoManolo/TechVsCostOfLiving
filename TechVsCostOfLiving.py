a00bfc0a7e8d86e61a67f8e2174361052cdaca4a
RaposoManolo <47564554+RaposoManolo@users.noreply.github.com>
2024-02-01 19:33:12 +0000

    Fully converted to Plotly n Dash

--- TechVsCostOfLiving.py
+++ TechVsCostOfLiving.py
@@ -1,25 +1,41 @@
 #import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
-#import numpy as np
-
-import dash
-from dash import html
-import dash_bootstrap_components as dbc
+import io
+import requests
+from dash import Dash, html, dcc, callback
 
 columns = ["Country", "AvgSalary", "Cost of Living Index"]
 
-
-
-
-df = pd.read_csv("TechCostOfLiving.csv", index_col=2, usecols=columns)
-
+#df = pd.read_csv("TechCostOfLiving.csv", index_col=2, usecols=columns)
+url ="https://raw.githubusercontent.com/RaposoManolo/TechVsCostOfLiving/main/TechCostOfLiving.csv"
+s=requests.get(url).content
+df = pd.read_csv(io.StringIO(s.decode('utf-8')), index_col=2, usecols=columns)
 #print(df)
-
-
 df = df.rename(columns={'AvgSalary': 'Avg Salary - Thousands (USD)'})
 
-fig = px.scatter(df, x="Avg Salary - Thousands (USD)", y="Cost of Living Index", text="Country", log_x=True, size_max=100)
-fig.update_traces(textposition='top center')
-fig.update_layout(title_text='Avg Tech Wage Vs Cost of Living', title_x=0.5)
-fig.show()
+## Web App Layout
+app = Dash(title="Tech Salaries")
+
+app.layout = html.Div(
+    children=[
+        html.H1("Avg Tech Salary Vs Cost of Living", style={"text-align": "center"}),
+        html.Br(),
+          html.Div(
+            children=[
+                    dcc.Graph(id="scatter_chart", figure=px.scatter(df, x="Avg Salary - Thousands (USD)", y="Cost of Living Index", text="Country", log_x=True, size_max=100))
+            ],
+            style={"display": "center", "width": "100%", "height": "100%"}
+        ),
+    ],
+    style={"padding": "0px"}
+)
+
+
+
+#fig.update_traces(textposition='top center')
+#fig.update_layout(title_text='Avg Tech Wage Vs Cost of Living', title_x=0.5)
+#fig.show()
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
