3bd42d28cf0bb1ce2c01fe6c954c1078209e9080
RaposoManolo <47564554+RaposoManolo@users.noreply.github.com>
2024-02-09 20:18:32 +0000

    Added bootstrap theme

--- TechVsCostOfLiving.py
+++ TechVsCostOfLiving.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import io
 import requests
 from dash import Dash, html, dcc, callback
+import dash_bootstrap_components as dbc
 
 columns = ["Country", "AvgSalary", "Cost of Living Index"]
 
@@ -15,7 +16,8 @@ df = pd.read_csv(io.StringIO(s.decode('utf-8')), index_col=2, usecols=columns)
 df = df.rename(columns={'AvgSalary': 'Avg Salary - Thousands (USD)'})
 
 ## Web App Layout
-app = Dash(title="Tech Salaries")
+app = Dash(title="Tech Salaries",
+    external_stylesheets=[dbc.themes.DARKLY])
 
 app.layout = html.Div(
     children=[
@@ -25,7 +27,7 @@ app.layout = html.Div(
             children=[
                     dcc.Graph(id="scatter_chart", figure=px.scatter(df, x="Avg Salary - Thousands (USD)", y="Cost of Living Index", text="Country", log_x=True, size_max=100))
             ],
-            style={"display": "center", "width": "100%", "height": "100%"}
+            style={"display": "center", "width": "100%", "height": "90%"}
         ),
     ],
     style={"padding": "0px"}
