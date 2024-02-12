5a763af9ef1e8687859c8fc988fbda3c2c23bb35
RaposoManolo <47564554+RaposoManolo@users.noreply.github.com>
2024-02-12 20:36:26 +0000

    Added tabs, added value in list from variable. Removed NAN value from data set

--- TechVsCostOfLiving.py
+++ TechVsCostOfLiving.py
@@ -1,4 +1,3 @@
-#import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 import io
@@ -6,38 +5,60 @@ import requests
 from dash import Dash, html, dcc, callback
 import dash_bootstrap_components as dbc
 
+#Set variables
 columns = ["Country", "AvgSalary", "Cost of Living Index"]
-
-#df = pd.read_csv("TechCostOfLiving.csv", index_col=2, usecols=columns)
 url ="https://raw.githubusercontent.com/RaposoManolo/TechVsCostOfLiving/main/TechCostOfLiving.csv"
 s=requests.get(url).content
 df = pd.read_csv(io.StringIO(s.decode('utf-8')), index_col=2, usecols=columns)
-#print(df)
 df = df.rename(columns={'AvgSalary': 'Avg Salary - Thousands (USD)'})
+df = df.apply(lambda x: pd.Series(x.dropna().values)) ###
+numberCountriesInScope= len(df["Country"])
 
-## Web App Layout
-app = Dash(title="Tech Salaries",
-    external_stylesheets=[dbc.themes.DARKLY])
+## Web App Set
+app = Dash(title="Tech Salaries", 
+           external_stylesheets=[dbc.themes.DARKLY])
 
+## Layout Set
 app.layout = html.Div(
     children=[
         html.H1("Avg Tech Salary Vs Cost of Living", style={"text-align": "center"}),
         html.Br(),
-          html.Div(
-            children=[
-                    dcc.Graph(id="scatter_chart", figure=px.scatter(df, x="Avg Salary - Thousands (USD)", y="Cost of Living Index", text="Country", log_x=True, size_max=100))
-            ],
-            style={"display": "center", "width": "100%", "height": "90%"}
+        html.Div(
+            dbc.Tabs([
+                dbc.Tab([  
+                        html.Br(),
+                        html.Div(
+                            children=[dcc.Graph(
+                                id="scatter_chart", 
+                                figure=px.scatter(df, x="Avg Salary - Thousands (USD)", y="Cost of Living Index", text="Country", log_x=True, size_max=100))
+                            ], style={"display": "center", "width": "100%", "height": "100%"}
+                            ),
+                        ], style={"padding": "0px"}
+                        ,label='Chart Analysis'),
+                dbc.Tab([
+                    html.Ul([
+                        html.Br(),
+                        html.Li('Author: Iuri Manuel Raposo'),
+                        html.Li(['GitHub repo: ',
+                        html.A('https://github.com/RaposoManolo/TechVsCostOfLiving/',
+                                href='https://github.com/RaposoManolo/TechVsCostOfLiving/')
+                                ]),
+                        html.Li(['# of countries in scope: '] + [numberCountriesInScope]),
+                        html.Li([
+                            'Data Source: ',
+                                html.A('https://www.numbeo.com/cost-of-living/region_prices_by_city?itemId=105&region=150',
+                                href='https://www.numbeo.com/cost-of-living/region_prices_by_city?itemId=105&region=150')
+                                ]) ,
+                        html.Li('Update Frequency: To be set'),
+                        html.Li('Last Updated:2024 Q1'),
+                        html.Li('Goal: Testing the capabilities of Python using the Plotly and Dash framework.'),           
+                            ])
+                        ], label='Project Info')
+            ]),
+           
         ),
-    ],
-    style={"padding": "0px"}
+    ],style={"padding": "0px"}
 )
 
-
-
-#fig.update_traces(textposition='top center')
-#fig.update_layout(title_text='Avg Tech Wage Vs Cost of Living', title_x=0.5)
-#fig.show()
-
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
